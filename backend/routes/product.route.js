10bbac1ac38029e280c90cae009979f98389edc9
synergisticgreed <sameergptnsr@gmail.com>
2025-04-13 00:32:24 +0530

    products routes added

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllProducts } from "../controllers/product.controller.js";
+import { createProduct, deleteProduct, getAllProducts , getFeaturedProducts, getProductsByCategory, getRecommendedProducts, toggleFeaturedProduct} from "../controllers/product.controller.js";
 import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
 
 
@@ -7,4 +7,13 @@ const router = express.Router();
 
 
 router.get("/",protectRoute,adminRoute,getAllProducts)
+router.get("/featured",getFeaturedProducts)
+router.get("/category/:category",getProductsByCategory)
+router.get("/recommendation", getRecommendedProducts)
+router.post("/", protectRoute, adminRoute ,createProduct);
+router.patch("/:id", protectRoute, adminRoute ,toggleFeaturedProduct);
+router.delete("/:id", protectRoute, adminRoute ,deleteProduct);
+
+
+
 export default router
