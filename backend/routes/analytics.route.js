490c31dcd4bb4ffc77c35af7fb81bd4926cd74f4
synergisticgreed <sameergptnsr@gmail.com>
2025-04-13 19:10:12 +0530

    analytics routes added

---
+++ backend/routes/analytics.route.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import { protectRoute, adminRoute } from '../middleware/auth.middleware.js';
+import { getAnalyticsData, getDailySalesData } from '../controllers/analytics.controller.js';
+
+
+const router = express.Router();
+
+router.get('/',protectRoute, adminRoute, async (req, res) => {
+    try {
+        const analyticsData = await getAnalyticsData();
+        
+        const endDate = new Date();
+        const startDate = new Date(endDate.getTime()-7*24*60*60*1000); //7 days ago
+        const dailySalesData = await getDailySalesData(startDate, endDate);
+        res.json({
+            analyticsData,
+            dailySalesData,
+        });
+    } catch (error) {
+        console.log("Error in analytics route", error.message);
+        res.status(500).json({ message: 'Server error', error: error.message });
+
+
+        
+    }
+})
+export default router;
