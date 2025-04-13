3c1c37b670e3303a021107188e77119c841dd64e
synergisticgreed <sameergptnsr@gmail.com>
2025-04-13 17:17:58 +0530

    payments routes added

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import authRoutes from './routes/auth.route.js';
 import productRoutes from './routes/product.route.js';
 import cartRoutes from './routes/cart.route.js';
 import couponRoutes from './routes/coupon.route.js';
+import paymentRoutes from './routes/payment.route.js';
 import { connectDB } from './lib/db.js';
 
 dotenv.config();
@@ -19,6 +20,7 @@ app.use("/api/auth",authRoutes);
 app.use("/api/products",productRoutes);
 app.use("/api/cart",cartRoutes);
 app.use("/api/coupons",couponRoutes);
+app.use("/api/payments",paymentRoutes);
 
 
 app.listen(PORT,()=>{
