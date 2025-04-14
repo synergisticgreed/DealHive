4707478d2579e92239114a1d4756e831e9be303a
synergisticgreed <sameergptnsr@gmail.com>
2025-04-14 15:39:06 +0530

    add createProduct and productLists functionality by admin

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app=express();
 
 const PORT=process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json({limit: "10mb"}));
 app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import { Toaster } from "react-hot-toast"
 import { useUserStore } from "./stores/useUserStore"
 import { useEffect } from "react"
 import LoadingSpinner from "./components/LoadingSpinner"
+import AdminPage from "./pages/AdminPage"
 
 function App() {
   const {user, checkAuth, checkingAuth} = useUserStore();
@@ -30,6 +31,7 @@ function App() {
         <Route path="/" element={<Homepage/>} />
         <Route path="/signup" element={!user ? <SignUpPage/> :<Navigate to='/' />} />
         <Route path="/login" element={!user ? <Loginpage/> :<Navigate to='/' />  } />
+        <Route path="/secret-dashboard" element={user?.role==="admin" ? <AdminPage/> :<Navigate to='/login' />  } />
 
 
       </Routes>
