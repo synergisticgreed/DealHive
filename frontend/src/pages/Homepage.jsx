559739ad393fb2ae6e441e87433a18817c5bbe6c
synergisticgreed <sameergptnsr@gmail.com>
2025-04-18 00:29:04 +0530

    implement featured products

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,5 +1,7 @@
+import { useEffect } from "react";
 import CategoryItem from "../components/CategoryItem";
-
+import { useProductStore } from "../stores/useProductStore";
+import FeaturedProducts from "../components/FeaturedProducts";
 
 const categories = [
 	{ href: "/jeans", name: "Jeans", imageUrl: "/jeans.jpg" },
@@ -11,8 +13,14 @@ const categories = [
 	{ href: "/bags", name: "Bags", imageUrl: "/bags.jpg" },
 ];
 
-const Homepage = () => {
-  return (
+const HomePage = () => {
+	const { fetchFeaturedProducts, products, isLoading } = useProductStore();
+
+	useEffect(() => {
+		fetchFeaturedProducts();
+	}, [fetchFeaturedProducts]);
+
+	return (
 		<div className='relative min-h-screen text-white overflow-hidden'>
 			<div className='relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16'>
 				<h1 className='text-center text-5xl sm:text-6xl font-bold text-emerald-400 mb-4'>
@@ -28,10 +36,9 @@ const Homepage = () => {
 					))}
 				</div>
 
-				
+				{!isLoading && products.length > 0 && <FeaturedProducts featuredProducts={products} />}
 			</div>
 		</div>
 	);
-}
-
-export default Homepage
+};
+export default HomePage;
