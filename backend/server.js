a243a309b2ba1716caa7fc25f8140df69151a93e
synergisticgreed <sameergptnsr@gmail.com>
2025-05-25 17:21:30 +0530

    path bug fixed

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,10 @@ const app=express();
 
 const PORT=process.env.PORT || 5000;
 
-const __dirname=path.resolve()
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 app.use(express.json({limit: "10mb"}));
 app.use(cookieParser());
