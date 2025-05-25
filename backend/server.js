475a94720bc05a3cdc42a060de4f1eaa73953974
synergisticgreed <sameergptnsr@gmail.com>
2025-05-25 17:04:18 +0530

    Project Completed

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import path from 'path';
+
 import authRoutes from './routes/auth.route.js';
 import productRoutes from './routes/product.route.js';
 import cartRoutes from './routes/cart.route.js';
@@ -14,6 +16,8 @@ const app=express();
 
 const PORT=process.env.PORT || 5000;
 
+const __dirname=path.resolve()
+
 app.use(express.json({limit: "10mb"}));
 app.use(cookieParser());
 
@@ -25,6 +29,13 @@ app.use("/api/payments",paymentRoutes);
 app.use("/api/analytics",analyticsRoutes);
 
 
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"/frontend/dist")));
+    app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
+    });
+}
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
     connectDB();
