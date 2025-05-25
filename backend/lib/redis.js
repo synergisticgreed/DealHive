cf88b788854bfbc8591511361b9a0865ae01b6b4
synergisticgreed <sameergptnsr@gmail.com>
2025-05-25 15:31:29 +0530

    update redis uri

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -1,8 +1,5 @@
 import Redis from "ioredis"
-import dotenv from "dotenv"
-
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 export const redis = new Redis(process.env.UPSTASH_REDIS_URL);
-
-//key-value store 
 await redis.set('foo', 'bar');
